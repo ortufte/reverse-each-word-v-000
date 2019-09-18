3d26043e8d3c2a266a14f9847bf9522d22f160b6
Olivia <ortufte@gmail.com>
2019-09-18 14:29:45 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  array.each {|word| word.reverse!}
+  array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  array.collect {|word| word.reverse!}
+  array.join(" ")
+end
+
